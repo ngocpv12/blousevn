35368089b75d9f523423cf0fdf79442d2f6cd01a
Ngoc Pham <phamngok913@gmail.com>
2020-08-26 01:26:41 +0700

    config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  SERVER_URL: 'http://localhost:3001/'
+  // SERVER_URL: 'http://localhost:3001/'
+  SERVER_URL: 'https://blouse-api.herokuapp.com/'
 };
 
 /*
